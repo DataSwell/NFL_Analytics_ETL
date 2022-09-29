7afb044bf788384dc146ec763249bd1c6e6b9351
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-09-28 18:08:26 -0600

    Create database_config.py

---
+++ src/database_config.py
@@ -0,0 +1,37 @@
+import os
+import psycopg2
+
+# password for the database from an environment variable
+db_pw = os.environ.get('DB_PASS')
+conn_str = "host=localhost user=postgres dbname=nfl_scores_bets password={}".format(db_pw)
+
+class MyDatabase():
+    def __init__(self):
+        self.conn = psycopg2.connect(conn_str)
+        self.cur = self.conn.cursor()
+
+        self.conn.set_session(autocommit=True)
+
+    def fetchall(self,query):
+        try:
+            self.cur.execute(query)
+        except psycopg2.Error as e:
+            print(e)
+        return self.cur.fetchall()
+
+    def fetchone(self, query):
+        try:
+            self.cur.execute(query)
+        except psycopg2.Error as e:
+            print(e)
+        return self.cur.fetchone()
+
+    def query_func(self, query, params=None):
+        try:
+            self.cur.execute(query)
+        except psycopg2.Error as e:
+            print(e)
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
