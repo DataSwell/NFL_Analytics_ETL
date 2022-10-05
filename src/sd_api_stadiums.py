b22c3895ad411e6991e035617287a858debc8a09
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-10-05 16:09:54 -0600

    Update sd_api_stadiums.py

--- src/sd_api_stadiums.py
+++ src/sd_api_stadiums.py
@@ -1,8 +1,7 @@
 import requests
 import os
 import pandas as pd
-
-from Projekte.NFL_Analytics.src.database_config import MyDatabase
+from database_config import MyDatabase
 
 # getting the API-Key which is stored in the environment variables 
 api_key = os.environ.get('SD_API_Key')
@@ -25,9 +24,24 @@ df_stadiums = df_stadiums.drop(['GeoLat', 'GeoLong'], axis=1)
 print(df_stadiums.head())
 print(df_stadiums.tail())
 
-# Loadig the newest Stadiums data to the data Folder
-#df_stadiums.to_excel(r'D:\Projekte\NFL_Analytics\data\sd_stadiums.xlsx', index=False, header=True)
+# Save the data local as a CSV file
+df_stadiums.to_csv('Projekte/Football_Analytics/data/SD_stadiums.csv', index=False)
+df_stadiums.to_excel('Projekte/Football_Analytics/data/SD_stadiums.xlsx', index=False)
 
 # Loading the data in the Database
-db = MyDatabase
+db = MyDatabase()
 
+insert_stadiums_string = """INSERT INTO sd_stadiums (
+stadiumID,
+stadium_name,
+stadium_city,
+stadium_state,
+stadium_country,
+stadium_capacity,
+stadium_surface,
+stadium_type) 
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+for i, row in df_stadiums.iterrows():
+    db.query_func(insert_stadiums_string, list(row))
