4a41f32da0d6bb0a437e439ae4119deee4b470d8
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-10-05 18:33:45 -0600

    Create sd_scores.py

---
+++ src/sd_scores.py
@@ -0,0 +1,82 @@
+import requests
+import os
+import pandas as pd
+from database_config import MyDatabase
+
+# getting the API-Key which is stored in the environment variables 
+api_key = os.environ.get('SD_API_Key')
+
+url = "https://api.sportsdata.io/v3/nfl/scores/json/Scores/{2022REG}"
+
+payload={}
+headers = {
+    'Ocp-Apim-Subscription-Key': f'{api_key}'
+}
+
+response = requests.request("GET", url, headers=headers, data=payload)
+res_json = response.json()
+df_scores = pd.DataFrame(res_json)
+
+
+# Transform
+df_scores = df_scores.filter(items=[
+'GameKey',
+'SeasonType',
+'Season',
+'Week',
+'Date',
+'AwayTeam',
+'HomeTeam',
+'AwayScore',
+'HomeScore',
+'PointSpread',
+'OverUnder',
+'StadiumID',
+'Day',
+'AwayTeamID',
+'HomeTeamID',
+'ScoreID',
+'Status'
+])
+
+# Replacing NaN Values to None which is equal to  NULL in postgres
+# the games which haven´t started can be filtered 
+df_scores = df_scores.fillna(0)
+
+print(df_scores.head())
+print(df_scores.tail())
+
+df_scores.to_csv('Projekte/Football_Analytics/data/SD_scores.csv', index=False)
+df_scores.to_excel('Projekte/Football_Analytics/data/SD_scores.xlsx', index=False)
+
+
+
+# Loading into Database
+db = MyDatabase()
+
+# fresh data must replace the data from the last week for the currant season
+# drop all rows where season_type = 1 and season = 2022 
+
+insert_scores = """INSERT INTO sd_scores (
+    game_key,
+    season_type,
+    season,
+    week,
+    date,
+    away_team,
+    home_team,
+    away_score,
+    home_score,
+    point_spread,
+    over_under,
+    stadiumID,
+    day,
+    away_teamID,
+    home_teamID,
+    scoreID,
+    status) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+for i, row in df_scores.iterrows():
+    db.query_func(insert_scores, list(row))
