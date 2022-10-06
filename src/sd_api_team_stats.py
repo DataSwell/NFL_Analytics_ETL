4b0a1dc300fa97e706d8bd1ec7a034cec6d66582
DataSwell <63445819+DataSwell@users.noreply.github.com>
2022-10-05 18:33:42 -0600

    Update sd_api_team_stats.py

--- src/sd_api_team_stats.py
+++ src/sd_api_team_stats.py
@@ -1,12 +1,13 @@
 import requests
 import os
 import pandas as pd
+from database_config import MyDatabase
 
 # getting the API-Key which is stored in the environment variables 
 api_key = os.environ.get('SD_API_Key')
 
 
-url = "https://api.sportsdata.io/v3/nfl/scores/json/TeamSeasonStats/{2021REG}"
+url = "https://api.sportsdata.io/v3/nfl/scores/json/TeamSeasonStats/{2022REG}"
 
 payload={}
 headers = {
@@ -19,11 +20,74 @@ print(response.text)
 res_json = response.json()
 df_team_season_stats = pd.DataFrame(res_json)
 
+
 # Transformation
-required_columns = list()
+df_team_season_stats = df_team_season_stats.filter(items=[
+'SeasonType', 
+'Season',
+'Team',
+'Score',
+'OpponentScore',
+'TotalScore',
+'ScoreQuarter1',
+'ScoreQuarter2',
+'ScoreQuarter3',
+'ScoreQuarter4',
+'ScoreOvertime',
+'TimeOfPossession',
+'OpponentScoreQuarter1',
+'OpponentScoreQuarter2',
+'OpponentScoreQuarter3',
+'OpponentScoreQuarter4',
+'OpponentScoreOvertime',
+'OpponentTimeOfPossession',
+'TimesSackedPercentage',
+'TeamName',
+'Games',
+'TeamSeasonID',
+'TeamID',
+'TeamStatID'
+])
 
 print(df_team_season_stats.head())
 print(df_team_season_stats.tail())
 
 df_team_season_stats.to_csv('Projekte/Football_Analytics/data/SD_team_season_stats.csv', index=False)
 df_team_season_stats.to_excel('Projekte/Football_Analytics/data/SD_team_season_stats.xlsx', index=False)
+
+# fresh data must replace the data from the last week for the currant season
+
+# Loading into Database
+db = MyDatabase()
+
+insert_team_stats = """INSERT INTO sd_team_stats (
+    season_type,
+    season,
+    team,
+    score,
+    opponent_score,
+    total_score,
+    score_q1,
+    score_q2,
+    score_q3,
+    score_q4,
+    score_overtime,
+    time_of_possession,
+    opponent_score_q1,
+    opponent_score_q2,
+    opponent_score_q3,
+    opponent_score_q4,
+    opponent_score_Overtime,
+    opponent_time_of_possession,
+    times_sacked_percentage,
+    team_name,
+    games,
+    team_seasonID,
+    teamID,
+    team_statID)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, 
+    %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+"""
+
+for i, row in df_team_season_stats.iterrows():
+    db.query_func(insert_team_stats, list(row))
